f71a4b4103b69c7faa5a03c41af4c59f103ae8a2
Roy McFarland <roy.mcfarland@gmail.com>
2015-06-16 22:46:37 -0600

    test github integration with electron

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var BrowserWindow = require("browser-window")
 
 app.on("ready", function() {
 	var mainWindow = new BrowserWindow({
-		width: 800,
-		height: 600
+		width: 1000,
+		height: 800
 	})
 	mainWindow.loadUrl("file://" + __dirname + "/index.html")
 })
