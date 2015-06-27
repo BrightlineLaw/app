0abc630d783f86e46fcc2d03bfc2cb1f6981c113
Roy McFarland <roy.mcfarland@gmail.com>
2015-06-26 21:57:59 -0600

    re-architect electron main.js; interpolate variables to test JS

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
-var app = require("app")
-var BrowserWindow = require("browser-window")
+var app = require('app');
+var BrowserWindow = require('browser-window'); // module to create native browser window
 
-app.on("ready", function() {
-	var mainWindow = new BrowserWindow({
-		width: 1000,
-		height: 800
-	})
-	mainWindow.loadUrl("file://" + __dirname + "/index.html")
-})
+// report crashes to server
+require('crash-reporter').start();
+
+// store global reference to the window object
+var mainWindow = null;
+
+// quit when all windows are closed
+app.on('window-all-closed', function() {
+	if (process.platform != 'darwin') {
+		app.quit();
+	}
+});
+
+// method called when electron has completed initialization and ready to create browser windows
+app.on('ready', function() {
+	
+	// create the browser window
+	mainWindow = new BrowserWindow({width: 1200, height: 1000});
+
+	// load index.html
+	mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+	// open devtools
+	mainWindow.openDevTools();
+
+	// when window is closed
+	mainWindow.on('closed', function() {
+		mainWindow = null;
+	});
+});
