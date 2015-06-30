09e67f328409759b8b1e35bd08f251fe4c06fde1
Roy McFarland <roy.mcfarland@gmail.com>
2015-06-29 23:13:36 -0600

    add buttons and goToDashboard navigation from profile & settings views

--- js/controllers/ProfileCtrl.js
+++ js/controllers/ProfileCtrl.js
@@ -3,4 +3,10 @@ var ProfileCtrl = angular.module('ProfileCtrl', []);
 ProfileCtrl.controller('ProfileController', ['$scope', '$state', function($scope, $state) {
 	var vm = this;
 	$scope.testMessage = "This is /profile";
+
+	// navigation
+	$scope.goToDashboard = function() {
+		$state.go('dashboard');
+	}
+
 }])
--- js/controllers/SettingsCtrl.js
+++ js/controllers/SettingsCtrl.js
@@ -3,4 +3,10 @@ var SettingsCtrl = angular.module('SettingsCtrl', []);
 SettingsCtrl.controller('SettingsController', ['$scope', '$state', function($scope, $state) {
 	var vm = this;
 	$scope.testMessage = "This is /settings";
+
+	// navigation
+	$scope.goToDashboard = function() {
+		$state.go('dashboard');
+	}
+
 }])
